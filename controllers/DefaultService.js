094a4b6455528ca358e92f4d043dda1b62577ee2
f-w <fred.wen@gov.bc.ca>
2016-01-27 14:11:26 -0800

    Customized API and supplied sample data (99 records).

--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -1,104 +1,52 @@
 'use strict';
 
-exports.propertiesGet = function(args, res, next) {
+var fs = require('fs');
+var path = require('path')
+
+exports.propertiesGet = function (args, res, next) {
   /**
    * parameters expected in the args:
    * address (String)
    * start (Integer)
    * count (Integer)
    **/
+  var exampleData = fs.readFileSync(path.join(__dirname, '../data/index.json'))
+  var examples = JSON.parse(exampleData)
 
-var examples = {};
-  
-  examples['application/json'] = [ {
-  "area" : "aeiou",
-  "rollNumFormatted" : "aeiou",
-  "jurisdictionDesc" : "aeiou",
-  "propertyAddress" : "aeiou",
-  "jurisdiction" : "aeiou",
-  "id" : "aeiou",
-  "rollNum" : "aeiou"
-} ];
-  
-
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
+  var resArr = examples.filter(function (e, i, a) {
+    return args.address.value && e.propertyAddress && e.propertyAddress.toLowerCase().indexOf(args.address.value.toLowerCase()) >= 0
+  })
+  var start = (args.start && args.start.value) || 0
+  var count = (args.count && args.count.value) || 10
+  resArr = resArr.slice(start, start + count)
+  resArr = resArr.map(function (e) {
+    return {
+      id: e.id,
+      area: e.area,
+      jurisdiction: e.jurisdiction,
+      jurisdictionDesc: e.jurisdictionDesc,
+      rollNum: e.rollNum,
+      rollNumFormatted: e.rollNumFormatted,
+      propertyAddress: e.propertyAddress
+    }
+  })
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(resArr))
 }
-exports.propertiesIdGet = function(args, res, next) {
+
+exports.propertiesIdGet = function (args, res, next) {
   /**
    * parameters expected in the args:
    * id (String)
    **/
 
-var examples = {};
-  
-  examples['application/json'] = {
-  "basementFinishArea" : "",
-  "rateCodeSummary" : "aeiou",
-  "numUnitApt" : "",
-  "postDirectional" : "aeiou",
-  "jurisdictionDesc" : "aeiou",
-  "commercialAdditionalBuildings" : true,
-  "cityCode" : "aeiou",
-  "jurisdiction" : "aeiou",
-  "postalCode" : "aeiou",
-  "unitNumber" : "aeiou",
-  "carports" : "",
-  "neighbourhoodCode" : "aeiou",
-  "yearBuilt" : "",
-  "streetName" : "aeiou",
-  "landSize" : "aeiou",
-  "province" : "aeiou",
-  "preDirectional" : "aeiou",
-  "strataTotalArea" : "",
-  "basementFinishAreaComm" : "",
-  "id" : "aeiou",
-  "schoolDistrictCode" : "aeiou",
-  "totalAssessedBuilding" : "",
-  "farmFlag" : true,
-  "rollNum" : "aeiou",
-  "basementFinishAreaIncm" : "",
-  "area" : "aeiou",
-  "streetType" : "aeiou",
-  "cityDesc" : "aeiou",
-  "residentialAdditionalBuildings" : true,
-  "streetNumber" : "aeiou",
-  "propertyAddress" : "aeiou",
-  "neighbourhoodDesc" : "aeiou",
-  "totalAssessedLand" : "",
-  "mobileHomeWidth" : "",
-  "mobileHomeLength" : "",
-  "bedrooms" : "",
-  "garages" : "",
-  "netLeasableArea" : "",
-  "baths" : "",
-  "totalAssessed" : "",
-  "storiesBuilding" : "",
-  "grossLeasableArea" : "",
-  "rollNumFormatted" : "aeiou",
-  "secondFloorArea" : "",
-  "firstFloorArea" : "",
-  "freeformAddress" : "aeiou",
-  "moblieHomeTotalArea" : ""
-};
-  
+  var exampleData = fs.readFileSync(path.join(__dirname, '../data/index.json'))
+  var examples = JSON.parse(exampleData)
 
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
+  var resArr = examples.filter(function (e, i, a) {
+    return args.id.value && e.id && e.id == args.id.value
+  })
+  var resObj = (resArr.length > 0) ? resArr[0] : {}
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(resObj))
 }
